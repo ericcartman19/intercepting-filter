3f0c9692a4919d981113adf30d1499f5b8c92ab7
bavaloyes <bavaloyes@ard01.mioct.ext.sopra>
2018-04-10 19:02:48 +0200

    implement the filter

--- intercepting-filter/src/com/valoyes/patterns/interceptingfilter/BrowserFilter.java
+++ intercepting-filter/src/com/valoyes/patterns/interceptingfilter/BrowserFilter.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 
 
 /**
  * Esta clase es la que implementara el Intercepting Filter pattern
- * Su trabajo especifico sera utilizar el parametro user-agent de la request
+ * Su trabajo especifico sera utilizar el parametro User-Agent de la request
  * para filtrarla
  *
  */
@@ -24,23 +26,29 @@ public class BrowserFilter implements Filter {
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
+		// se llama justo antes que el Filter es deleted
 	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-		// TODO Auto-generated method stub
-		// place your code here
 
-		// pass the request along the filter chain
+		String usertAgentHeader = ((HttpServletRequest) request).getHeader("User-Agent");
+		// usamos contains en vez de equals, pq este parametro contiene otra informacion
+		// ademas del navegador
+		if(usertAgentHeader.contains("Chrome")) {
+			chain.doFilter(request, response);
+		}else {
+			RequestDispatcher dispatcher = request.getRequestDispatcher("badBrowser.jsp");
+			dispatcher.forward(request, response);
+		}
+		
 		chain.doFilter(request, response);
 	}
 
-	/**
-	 * @see Filter#init(FilterConfig)
-	 */
 	public void init(FilterConfig fConfig) throws ServletException {
+		// este metodo se llama once the filter is created by the Servlet Container, durante el creation process
 	}
 
 }
